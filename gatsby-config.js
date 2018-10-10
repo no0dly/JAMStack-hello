fdb807370fe3b32844b9afb0f1d5a7ec31ee3ce0
Vadim Dermanovskiy <thenoodly@gmail.com>
2018-10-09 22:16:19 -0700

    changed text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Please!!',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,10 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hello World</h1>
+    <h1>
+      Hello everybody, I would like to participate in hackaton, please give me a
+      ticket!
+    </h1>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
